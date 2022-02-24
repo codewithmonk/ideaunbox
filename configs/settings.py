248279e7f56d734c518a1f5ba7c00e80c1bd1c82
Akash <codewithmonk@gmail.com>
2022-02-24 19:50:48 +0000

    adding postgres backend

--- configs/settings.py
+++ configs/settings.py
@@ -63,9 +63,13 @@ WSGI_APPLICATION = 'configs.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DB_DATABASE"),
+        "USER": os.getenv("DB_USERNAME"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": "db",
+        "PORT": 5432,
     }
 }
 
