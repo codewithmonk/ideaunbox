75e987cbf25b2e04ddff92424a1d9f2485325359
Akash Raj <codewithmonk@gmail.com>
2022-03-03 22:32:57 +0530

    adding homepage and homepage view

--- configs/settings.py
+++ configs/settings.py
@@ -46,7 +46,9 @@ ROOT_URLCONF = 'configs.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            str(BASE_DIR.joinpath('templates')),
+            ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
