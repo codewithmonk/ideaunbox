f89693341332bac2d7eeea967c0f8cbbf3e7c8c4
Akash <codewithmonk@gmail.com>
2022-03-07 18:41:40 +0000

    creating views for home, apparels and awards

--- core/views.py
+++ core/views.py
@@ -4,3 +4,9 @@ from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
     template_name = 'home.html'
+
+class ApparelsView(TemplateView):
+    template_name = 'home.html'
+
+class AwardView(TemplateView):
+    template_name = 'home.html'
