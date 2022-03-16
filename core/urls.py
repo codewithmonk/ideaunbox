e6ec3c1cb8da0dc1fca5a5bc8c98aa145a4f7573
Akash <codewithmonk@gmail.com>
2022-03-16 17:47:35 +0000

    adding product view and url routes

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from core.views import AwardView, ApparelsView
-from core.views import get_home_page, get_apparels
+from core.views import get_home_page, get_apparels, get_individual_item
 
 urlpatterns = [
     path('', get_home_page, name="home"),
     path('apparels/', get_apparels, name="apparels"),
     path('awards/', AwardView.as_view(), name="home"),
+    path('apparels/product/<slug>', get_individual_item, name="individual"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
+from pydoc import pager
 from django.shortcuts import render
 from django.views.generic import TemplateView
-from core.helpers import get_top_apparels, get_catalogue_items, get_filters
+from core.helpers import get_top_apparels, get_catalogue_items, get_filters, get_product_detail
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 # Create your views here.
 
@@ -11,20 +12,30 @@ def get_home_page(request):
     }
     return render(request, 'home.html', context)
 
-def get_apparels(request, filters=None):
-    items = get_catalogue_items("apparels", filters)
-    page = request.GET.get('page', 1)
+def paginate_items(items, request):
     paginator = Paginator(items, 20)
+    page = request.GET.get('page', 1)
     try:
         records = paginator.page(page)
     except PageNotAnInteger:
         records = paginator.page(1)
     except EmptyPage:
         records = paginator.page(paginator.num_pages)
-    # context['items'] = records
+    return records
+
+
+def get_apparels(request, filters=None):
+    # TODO: implement filtering and object querying
+    items = get_catalogue_items("apparels", filters)
+    records = paginate_items(items, request)
     context = {"items": records}
     # TODO: return filters available for each category vs applied
     return render(request, 'catalogue.html', context)
+
+def get_individual_item(request, slug):
+    item = get_product_detail(slug)
+    context = {"item": item}
+    return render(request, 'product.html', context)
 
 
 class ApparelsView(TemplateView):
